4f8df516b01ec3467e9f6503f8931307f7ba2204
vlew474 <122119223+vlew474@users.noreply.github.com>
2023-03-01 15:57:17 -0500

    Update main.js
    made it so that the outputted code is a random object from the array

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const message = 'Welcome! Please enter your phrase for submission.' // welcome message
 
+const codes = ['FSPDHJW', 'GWJRTOC', 'YWJMFIK', 'PWUNFJU'];
+
+const random = Math.floor(Math.random() * codes.length);
+
 function phrase(string = 'Please enter a valid phrase.') {
   if (string === '$SOL is 4 poorz') {
-    return 'FSPDHJW';
+    return console.log(codes[random]);
   } else if (string === 'SOL is 4 poors') {
-    return 'GWJRTOC';
+    return console.log(codes[random]);
   } else if (string === 'sol is for poors') {
-    return 'YWJMFIK';
+    return console.log(codes[random]);
   } else if (string === 'sol is for poorz') {
-    return 'PWUNFJU';
+    return console.log(codes[random]);
   } else {
-    return 'Please enter a valid phrase.';
+    return 'Please enter a valid phrase';
   }
 }
 
 // Log to console
 console.log(message);
-console.log(phrase('$SOL is 4 poorz'));
+console.log(phrase('SOL is 4 poors'))
